ffc43753172e7076508b86adf6324244d7b3b3dd
Fatin Ishraq Prapya <prapyakhan@gmail.com>
2022-03-20 01:43:34 +0600

    All File Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadToDo></LoadToDo>
     </div>
   );
+}
+
+function LoadToDo() {
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(data => data.json())
+      .then(response => console.log(response))
+  })
 }
 
 export default App;
